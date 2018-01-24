2198867b584eb1a7c87d5507a73cb26bce20ae60
adekrisna <32455883+adekrisna@users.noreply.github.com>
2018-01-25 03:23:25 +0700

    Update KrSelf.py

--- KrSelf.py
+++ KrSelf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import LINETCR
-from LINETCR.lib.curve.ttypes import *
+import KRIS
+from KRIS.lib.curve.ttypes import *
 from datetime import datetime
 import time, random, sys, ast, re, os, io, json, subprocess, threading, string, codecs, requests, ctypes, urllib, urllib2, urllib3, wikipedia, tempfile
 from bs4 import BeautifulSoup
@@ -12,7 +12,7 @@ from threading import Thread
 from gtts import gTTS
 from googletrans import Translator
 
-kr = LINETCR.LINE()
+kr = KRIS.LINE()
 #kr.login(qr=True)
 kr.login(token='')#
 kr.loginResult()
